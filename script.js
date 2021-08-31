95da2cced5334a75de97f3c2de5186ce133373e1
Whoistolu <digittolu25@gmail.com>
2021-08-31 12:00:57 +0100

    Add more logic to the jvascript part

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const mobileMenuDiv = document.querySelector('#mobile-menu');
-mobileMenuDiv.className = 'popup';
+mobileMenuDiv.className = 'mobile-menu-hidden';
 let anchorTag = document.createElement('a');
 anchorTag.setAttribute('href', '/');
+anchorTag.style.display = 'flex';
+anchorTag.style.justifyContent = 'flex-end';
+anchorTag.style.marginBottom = '10%';
+anchorTag.style.marginRight = '10%'
 let closingIcon = document.createElement('IMG');
 closingIcon.setAttribute('src', 'img/Icon.png');
 closingIcon.setAttribute('alt', 'icon image');
+closingIcon.id = 'close-button';
 anchorTag.appendChild(closingIcon);
 mobileMenuDiv.appendChild(anchorTag);
 anchorTag = document.createElement('a');
 anchorTag.textContent = 'Portfolio';
+anchorTag.setAttribute('href', '#actual-work-section');
+anchorTag.className = 'menu-links';
 mobileMenuDiv.appendChild(anchorTag);
 anchorTag = document.createElement('a');
 anchorTag.textContent = 'About';
+anchorTag.setAttribute('href', '#menu-about');
+anchorTag.className = 'menu-links';
 mobileMenuDiv.appendChild(anchorTag);
 anchorTag = document.createElement('a');
 anchorTag.textContent = 'Contact';
+anchorTag.setAttribute('href', '#menu-contact');
+anchorTag.className = 'menu-links';
 mobileMenuDiv.appendChild(anchorTag);
 let menuButton = document.querySelector('#menu-button');
-
-
-// function myFunction() { var x = document.getElementById("mobile-menu");
-// if (x.style.display === "block") { x.style.display = "none";
-//  } else { 
-//      x.style.display = "block"; } } ;
-// menuButton.addEventListener('click',myFunction);
