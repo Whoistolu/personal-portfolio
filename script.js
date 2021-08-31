fcac97453c411a7672441fa5752bf9e038945088
Gentilitysp <chimaijemeze@gmail.com>
2021-08-31 12:07:43 +0100

    Add functions to script.js

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ anchorTag.setAttribute('href', '#menu-contact');
 anchorTag.className = 'menu-links';
 mobileMenuDiv.appendChild(anchorTag);
 let menuButton = document.querySelector('#menu-button');
+
+function display () {
+  const targetedDiv = document.querySelector('.mobile-menu-hidden');
+  targetedDiv.classList.add('show');
+  menuButton.style.display = 'none';
+}
+menuButton.addEventListener('click',display);
+const closeButton = document.querySelector('#close-button');
+function closePop () {
+  const targetedDiv2 = document.querySelector('.mobile-menu-hidden');
+  targetedDiv2.classList.remove('show');
+  menuButton.style.display = 'block';
+}
+closeButton.addEventListener('click',closePop);
+const anchorTagLinks = document.querySelectorAll('.menu-links');
+anchorTagLinks.forEach((link) => {
+  link.addEventListener('click', closePop);
+})
