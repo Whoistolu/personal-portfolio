5448431c6f4ae30f1a6e13113eb760feb254841c
Whoistolu <digittolu25@gmail.com>
2021-09-01 18:50:11 +0100

    Add morelogic to the JS

--- script.js
+++ script.js
@@ -47,3 +47,50 @@ const anchorTagLinks = document.querySelectorAll('.menu-links');
 anchorTagLinks.forEach((link) => {
   link.addEventListener('click', closePop);
 });
+
+
+// popup window
+
+const details = [
+  {
+    name: 'Multi School Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and typesetting industry. Lorem Ipsum has been the industry and typesetting industry. Lorem Ipsum has been the industry and typesetting industry. Lorem Ipsum has been the industry',
+    image: 'img/ten.png',
+    technologies: ['ruby', 'css', 'javascript', 'bootsrap'],
+    live: 'https://music.youtube.com/watch?v=p45hoiFgge8&list=RDAMVMb9bn-EtmvvY',
+    source: 'https://music.youtube.com/watch?v=p45hoiFgge8&list=RDAMVMb9bn-EtmvvY',
+  },
+  
+];
+
+const SeeProjectButton = document.querySelectorAll('.project-button2');
+
+SeeProjectButton.forEach(function (item, i) {
+  item.addEventListener('click', function () {
+    const windowPop = document.querySelector('#modal');
+    windowPop.style.display = 'block';
+    const projectDetail = details[i];
+    const heading = document.querySelector('.heading h2');
+    heading.textContent = projectDetail.name;
+    const paragraph = document.querySelector('.img-p-button .description');
+    paragraph.textContent = projectDetail.description;
+    const picture = document.querySelector('.img-p-button .image');
+    picture.src = projectDetail.image;
+    const languages = document.querySelectorAll('.languages .bttn');
+    languages.forEach(function (li, s) {
+      li.textContent = projectDetail.technologies[s];
+    })
+    const live = windowPop.querySelector('.first-button');
+    live.href = projectDetail.live;
+    const source = windowPop.querySelector('second-button');
+    source.href = projectDetail.source;
+  });
+})
+
+const closeProjectButton = document.querySelector('.close');
+
+closeProjectButton.addEventListener('click', function(){
+  const windowPop = document.querySelector('#modal');
+    windowPop.style.display = 'none';
+})
+
