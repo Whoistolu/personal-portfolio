17a41c29a65699c4df8b2cde68624ac0cc254433
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-02 17:56:19 +0100

    Create variables that targets form and email input in form.js file as well as create span element in index.html file

--- form.js
+++ form.js
@@ -1,1 +1,3 @@
 const emailAddress = document.getElementById('user_email');
+const contactForm = document.getElementById('contact-details');
+const validatorMessage = document.getElementById('message-validator');
