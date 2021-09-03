d680ebbf2c90f93acc1f8ca0e54c566c3d688cdb
Whoistolu <digittolu25@gmail.com>
2021-09-03 17:22:12 +0100

    Implement validation that the email field has to be in lowercase

---
+++ form.js
@@ -0,0 +1,11 @@
+const emailAddress = document.getElementById('mail');
+const contactForm = document.getElementById('contact-details');
+const validatorMessage = document.getElementById('message-validator');
+contactForm.addEventListener('submit', (e) => {
+  if (emailAddress.value !== emailAddress.value.toLowerCase()) {
+    e.preventDefault();
+    validatorMessage.textContent = 'Kindly input email in lowercase!';
+  } else {
+    validatorMessage.textContent = '';
+  }
+});
