e6290e38bc6481c3c2a93186ba0f8fca2d510dd3
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-04 14:35:15 +0100

    Create a storage variable to store data inputted in the form controls

--- storage.js
+++ storage.js
@@ -3,3 +3,11 @@ const fullName = document.getElementById('user_name');
 const textMessage = document.getElementById('user_message');
 const templateForm = document.getElementById('form');
 const validatorMessage = document.getElementById('validator-message');
+templateForm.addEventListener('input', () => {
+    const localStorageObject = {
+      name: fullName.value,
+      email: emailAddress.value,
+      message: textMessage.value,
+    };
+    localStorage.setItem('templateFormData', JSON.stringify(localStorageObject));
+  });
