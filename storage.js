fd04928909551ca25c06048b31f43f9a14667af3
Whoistolu <digittolu25@gmail.com>
2021-09-04 14:14:39 +0100

    Declare variables for the user email, user name, user message, form and validator message

---
+++ storage.js
@@ -0,0 +1,5 @@
+const emailAddress = document.getElementById('user_email');
+const fullName = document.getElementById('user_name');
+const textMessage = document.getElementById('user_message');
+const templateForm = document.getElementById('form');
+const validatorMessage = document.getElementById('validator-message');
