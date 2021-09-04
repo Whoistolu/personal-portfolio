6a589085cad82400c9708735f5b631ff548eabf9
Whoistolu <digittolu25@gmail.com>
2021-09-04 01:10:17 +0100

    Fixmore  linter errors

--- form.js
+++ form.js
@@ -4,8 +4,8 @@ const validatorMessage = document.getElementById('validator-message');
 templateForm.addEventListener('submit', (e) => {
   if (emailAddress.value !== emailAddress.value.toLowerCase()) {
     e.preventDefault();
-  validatorMessage.textContent = 'Kindly input email in lowercase!';
+    validatorMessage.textContent = 'Kindly input email in lowercase!';
   } else {
-validatorMessage.textContent = '';
+  validatorMessage.textContent = '';
     }
-  });
+});
